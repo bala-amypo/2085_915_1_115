81fcebd6a7242cec175155a86806d3eb99eb3ac2
bala-amypo <bala@amypo.in>
2025-12-20 12:32:27 +0000

    User commit

--- src/main/java/com/example/demo/entity/TemperatureRule.java
+++ src/main/java/com/example/demo/entity/TemperatureRule.java
@@ -1,68 +1,81 @@
 package com.example.demo.entity;
 
+import jakarta.persistence.*;
 import java.time.LocalDate;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
-
 @Entity
-@Table(name="temperature_rules")
+@Table(name = "temperature_rules")
 public class TemperatureRule {
-    @Id@GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String productType;
     private Double minTemp;
     private Double maxTemp;
     private boolean active;
     private LocalDate effectiveFrom;
     private LocalDate effectiveTo;
+
+    public TemperatureRule() {}
+
     public Long getId() {
         return id;
     }
+
     public void setId(Long id) {
         this.id = id;
     }
+
     public String getProductType() {
         return productType;
     }
+
     public void setProductType(String productType) {
         this.productType = productType;
     }
+
     public Double getMinTemp() {
         return minTemp;
     }
+
     public void setMinTemp(Double minTemp) {
         this.minTemp = minTemp;
     }
+
     public Double getMaxTemp() {
         return maxTemp;
     }
+
     public void setMaxTemp(Double maxTemp) {
         this.maxTemp = maxTemp;
     }
+
     public boolean isActive() {
         return active;
     }
+
     public void setActive(boolean active) {
         this.active = active;
     }
+
     public LocalDate getEffectiveFrom() {
         return effectiveFrom;
     }
+
     public void setEffectiveFrom(LocalDate effectiveFrom) {
         this.effectiveFrom = effectiveFrom;
     }
+
     public LocalDate getEffectiveTo() {
         return effectiveTo;
     }
+
     public void setEffectiveTo(LocalDate effectiveTo) {
         this.effectiveTo = effectiveTo;
     }
-    public TemperatureRule() {
-    }
+
     public TemperatureRule(Long id, String productType, Double minTemp, Double maxTemp, boolean active,
             LocalDate effectiveFrom, LocalDate effectiveTo) {
         this.id = id;
@@ -73,5 +86,6 @@ public class TemperatureRule {
         this.effectiveFrom = effectiveFrom;
         this.effectiveTo = effectiveTo;
     }
+
     
 }
