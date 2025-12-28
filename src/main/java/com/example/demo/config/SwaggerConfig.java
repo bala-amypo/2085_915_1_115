2b2b0c75e93e541d16ba6a3226ba149e6ee2fa98
bala-amypo <bala@amypo.in>
2025-12-28 09:43:00 +0000

    User commit

--- src/main/java/com/example/demo/config/SwaggerConfig.java
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -17,12 +17,10 @@ public class SwaggerConfig {
     public OpenAPI customOpenAPI() {
 
         return new OpenAPI()
-                // ✅ FORCE HTTPS SERVER (CRITICAL FIX)
                 .servers(List.of(
-                        new Server().url("https://9105.408procr.amypo.ai")
+                        new Server().url("https://9099.408procr.amypo.ai")
                 ))
 
-                // ✅ JWT SECURITY
                 .addSecurityItem(
                         new SecurityRequirement().addList("bearerAuth")
                 )
